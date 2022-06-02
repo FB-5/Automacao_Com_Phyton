ad7449146309778665ddb8d742f421b107fd1fad
Filipe Bayao <81266323+FB-5@users.noreply.github.com>
2022-06-01 22:56:44 -0300

    Update Enviar e-mail com Python.py

--- Aula 2/Enviar e-mail com Python.py
+++ Aula 2/Enviar e-mail com Python.py
@@ -6,15 +6,16 @@ outlook = win32.Dispatch('outlook.application')
 # criar um email
 email = outlook.CreateItem(0)
 
+# aqui abaixo funções de cálculo
 faturamento = 1500
 qtde_produtos = 10
 ticket_medio = faturamento / qtde_produtos
 
 # configurar as informações do seu e-mail
-email.To = "pythonimpressionador@gmail.com; pythonimpressionador+lira@gmail.com"
+email.To = "teste@gmail.com; teste2@gmail.com"
 email.Subject = "E-mail automático do Python"
 email.HTMLBody = f"""
-<p>Olá Lira, aqui é o código Python</p>
+<p>Olá XX, aqui é o código Python</p>
 
 <p>O faturamento da loja foi de R${faturamento}</p>
 <p>Vendemos {qtde_produtos} produtos</p>
@@ -24,7 +25,7 @@ email.HTMLBody = f"""
 <p>Código Python</p>
 """
 
-# anexo = "C://Users/joaop/Downloads/arquivo.xlsx"
+# anexo = "C://Users/fulano/Downloads/arquivo.xlsx"
 # email.Attachments.Add(anexo)
 
 email.Send()
